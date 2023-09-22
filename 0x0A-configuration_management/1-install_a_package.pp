c3cd702daf750c225eb266a9c1de4a754e3eb6ea
Ilyass <ilias.kassimi@gmail.com>
2023-09-22 21:53:14 +0000

    2-1-execute_a-command.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
-# kill process killmenow
-
-exec { 'pkill':
-  command  => 'pkill killmenow',
-  provider => 'shell',
+#!/usr/bin/pup
+# install flask
+package {'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3'
 }
