3c7c342c6ced0f779e0453e17e3144bc5431386a
ilyass El filah <ilyasselfilal@192.168.1.23>
2024-02-14 02:57:35 +0100

    1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,9 +1,18 @@
 #!/usr/bin/python3
-import sys
+""" 1. Top Ten """
+from requests import get
 
-if __name__ == '__main__':
-    top_ten = __import__('1-top_ten').top_ten
-    if len(sys.argv) < 2:
-        print("Please pass an argument for the subreddit to search.")
-    else:
-        top_ten(sys.argv[1])
+
+def top_ten(subreddit):
+    """ function that queries the Reddit API and prints the titles
+    of the first 10 hot posts listed for a given subreddit. """
+    headers = {"User-Agent": "javierandresgp"}
+    try:
+        general = get("https://www.reddit.com/r/{}.json?limit=10&sort=hot"
+                      .format(subreddit), headers=headers,
+                      allow_redirects=False).json()
+        specific = general.get("data").get("children")
+        for i in specific:
+            print(i.get("data").get("title"))
+    except:
+        print(None)
