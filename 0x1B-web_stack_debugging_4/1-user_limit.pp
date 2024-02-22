38c8c142c08580f10f0b2c568b41babbaaebb287
ilyass El filah <ilyasselfilal@ilyasss-MacBook-Pro.local>
2024-02-23 00:54:27 +0100

    add tasks

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,12 +1,11 @@
-# fix nginx to accept and serve more requests
+# 1. User limit
+exec {'replace-hard':
+  provider => shell,
+  command  => 'sudo sed -i "s/holberton hard nofile 5/holberton hard nofile 4096/" /etc/security/limits.conf',
+  before   => Exec['replace-soft'],
+}
 
-exec { 'fix--for-nginx':
-  command => 'sed -i "s/15/4096/" /etc/default/nginx',
-  path    => '/usr/local/bin/:/bin/'
-} ->
-
-# Restart Nginx
-exec { 'nginx-restart':
-  command => 'nginx restart',
-  path    => '/etc/init.d/'
+exec {'replace-soft':
+  provider => shell,
+  command  => 'sudo sed -i "s/holberton soft nofile 4/holberton soft nofile 1024/" /etc/security/limits.conf',
 }
